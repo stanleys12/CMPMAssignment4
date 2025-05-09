71f550b48c5194c50acff400aee693a4c8eedf93
Stanley Shen <stanleyshen@Stanleys-MacBook-Pro.local>
2025-05-08 23:03:01 -0700

    Initial commit

--- Assets/Scripts/AI/BehaviorTree/Selector.cs
+++ Assets/Scripts/AI/BehaviorTree/Selector.cs
@@ -1,11 +1,35 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Selector : InteriorNode
 {
     public override Result Run()
     {
+        while (current_child < children.Count)
+        {
+            Result res = children[current_child].Run();
+            Debug.Log($"Selector evaluating child {current_child}: {res}");
+
+            if (res == Result.SUCCESS)
+            {
+                current_child = 0;  // Reset child index
+                Debug.Log("Selector returning SUCCESS.");
+                return Result.SUCCESS;
+            }
+            if (res == Result.IN_PROGRESS)
+            {
+                Debug.Log("Selector returning IN_PROGRESS.");
+                return Result.IN_PROGRESS;
+            }
+            
+            current_child++;
+        }
+
+        current_child = 0;  // Reset child index if all fail
+        Debug.Log("Selector returning FAILURE.");
         return Result.FAILURE;
     }
+
 
     public Selector(IEnumerable<BehaviorTree> children) : base(children)
     {
@@ -15,5 +39,4 @@ public class Selector : InteriorNode
     {
         return new Selector(CopyChildren());
     }
-
 }
